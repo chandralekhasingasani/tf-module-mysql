b81edd4ae5101684ddea9d877cb51cc419403d8c
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-23 19:07:11 +0530

    test

--- subnet-group.tf
+++ subnet-group.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "default" {
   name       = "${var.ENV}"
-  subnet_ids = [var.SUBNET_IDS]
+  subnet_ids = var.SUBNET_IDS
 
   tags = {
     Name = "${var.ENV}-${var.COMPONENT}"
