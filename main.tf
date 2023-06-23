7d6b4b4dfb039c6514e37781bf28ba2f1bc23d83
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-23 19:11:20 +0530

    test

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "default" {
   engine               = var.ENGINE
   engine_version       = var.ENGINE_VERSION
   instance_class       = var.INSTANCE_CLASS
-  username             = jsondecode(aws_secretsmanager_secret_version.roboshop.secret_string)["RDS_USERNAME"]
-  password             = jsondecode(aws_secretsmanager_secret_version.roboshop.secret_string)["RDS_PASSWORD"]
+  username             = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["RDS_USERNAME"]
+  password             = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)["RDS_PASSWORD"]
   parameter_group_name = aws_db_parameter_group.default.name
   skip_final_snapshot  = true
 }
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "roboshop" {
   arn = "arn:aws:secretsmanager:us-east-1:697140473466:secret:roboshop-3wTSpx"
 }
 
-resource "aws_secretsmanager_secret_version" "roboshop" {
+data "aws_secretsmanager_secret_version" "roboshop" {
   secret_id     = data.aws_secretsmanager_secret.roboshop.id
   secret_string = "roboshop"
 }
